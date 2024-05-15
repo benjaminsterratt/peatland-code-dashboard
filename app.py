3c81ec3bf96a58adf8dc90bca6105fb2bb10e8b1
benjaminsterratt <151463115+benjaminsterratt@users.noreply.github.com>
2024-05-16 00:02:16 +0100

    Data changes; added filter options.

--- app.py
+++ app.py
@@ -2,26 +2,59 @@ from shiny import run_app, App, reactive, ui
 
 import pandas as pd
 
-DATA = pd.read_csv("data.csv")
+#%% GLOBALS
+
+DATA = pd.read_csv("data.csv", keep_default_na = False)
+
+DATA["Country"] = DATA["Country"].astype(pd.CategoricalDtype(["England", "Scotland", "Wales", "Northern Ireland"], ordered = True))
+DATA["Status"] = DATA["Status"].astype(pd.CategoricalDtype(["Under Development", "Validated", "Restoration Validated"], ordered = True))
+DATA["PIUs Listed"] = DATA["PIUs Listed"].astype(pd.CategoricalDtype(["Yes", "No"], ordered = True))
+
+GROUPING_COLUMNS = ["Type", "Country", "Developer", "Validator", "Status", "PIUs Listed"]
+
+#%% FUNCTIONS
+
+def ui_filter(columnName):
+    choices = list(DATA[columnName].sort_values().unique())
+    if len(choices) < 5:
+        return ui.accordion_panel(
+            columnName,
+            ui.input_checkbox_group(
+                "filter_" + columnName.replace(" ", "_"), 
+                None, 
+                choices,
+                selected = choices)
+            )
+    else:
+        return ui.accordion_panel(
+            columnName,
+            ui.div(
+                ui.input_select(
+                    "filter_" + columnName.replace(" ", "_"), 
+                    None, 
+                    choices,
+                    selected = choices,
+                    multiple = True,
+                    size = round(len(choices)/2)
+                    )
+                )
+            )
+
+#%% UI
 
 userInterface = ui.page_sidebar(
     ui.sidebar(
         ui.accordion(
             ui.accordion_panel("Breakdown",
-                               ui.input_radio_buttons("groupby", None, ["Type", "Country", "Developer", "Validator", "Status", "PIUs Listed"]),
+                               ui.input_radio_buttons("groupby", None, GROUPING_COLUMNS),
                                ),
             ui.accordion_panel("Filters",
                                ui.accordion(
-                                   ui.accordion_panel("Type"),
-                                   ui.accordion_panel("Country"),
-                                   ui.accordion_panel("Developer"),
-                                   ui.accordion_panel("Validator"),
-                                   ui.accordion_panel("Status"),
-                                   ui.accordion_panel("PIUs Listed")
-                                   )
-                               )
-            ),
-        title = "Peatland Code Dashboard"),
+                                   *[ui_filter(column) for column in GROUPING_COLUMNS],
+                                   open = False)
+                               ),
+            open = True),
+        width = 400, title = "Peatland Code Dashboard"),
     ui.layout_columns(
         ui.layout_columns(
             ui.card(
@@ -34,13 +67,15 @@ userInterface = ui.page_sidebar(
                 ui.card_header("Area Breakdown")
                 ),
             col_widths = [12, 6, 6]),
-        ui.navset_card_underline(
+        ui.navset_card_pill(
             ui.nav_panel("List"),
             ui.nav_panel("Map"),
             title = "Projects"
             ),
         col_widths = [8, 4]),
     fillable = True)
+
+#%% SERVER
 
 def server(input):
     pass
