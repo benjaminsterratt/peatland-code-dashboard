95ceaa2767b0cde51eb148aaffc86a274a6cb342
benjaminsterratt <151463115+benjaminsterratt@users.noreply.github.com>
2024-05-17 20:31:42 +0100

    Added sidebar de/select all and reset buttons.

--- app.py
+++ app.py
@@ -16,29 +16,25 @@ GROUPING_COLUMNS = ["Type", "Country", "Developer", "Validator", "Status", "PIUs
 
 def ui_filter(columnName):
     choices = list(DATA[columnName].sort_values().unique())
-    if len(choices) < 5:
-        return ui.accordion_panel(
-            columnName,
-            ui.input_checkbox_group(
-                "filter_" + columnName.replace(" ", "_"), 
-                None, 
-                choices,
-                selected = choices)
+    elements = [
+        ui.input_checkbox_group(
+            "filter_" + columnName.replace(" ", "_"), 
+            None, 
+            choices,
+            selected = choices
             )
-    else:
-        return ui.accordion_panel(
-            columnName,
-            ui.div(
-                ui.input_select(
-                    "filter_" + columnName.replace(" ", "_"), 
-                    None, 
-                    choices,
-                    selected = choices,
-                    multiple = True,
-                    size = round(len(choices)/2)
-                    )
-                )
-            )
+        ]
+    if len(choices) > 4:
+        elements = [
+            ui.layout_columns(
+                ui.input_action_button("filter_" + columnName.replace(" ", "_") + "_selectAll", "Select all"),
+                ui.input_action_button("filter_" + columnName.replace(" ", "_") + "_deselectAll", "Deselect all"),
+                style = "margin-bottom: 32px;")
+            ] + elements
+    return ui.accordion_panel(
+        columnName,
+        *elements
+        )
 
 #%% UI
 
@@ -49,12 +45,13 @@ userInterface = ui.page_sidebar(
                                ui.input_radio_buttons("groupby", None, GROUPING_COLUMNS),
                                ),
             ui.accordion_panel("Filters",
+                               ui.input_action_button("resetFilters", "Reset filters", style = "margin-left: 20px; margin-right: 20px; margin-bottom: 32px;"),
                                ui.accordion(
                                    *[ui_filter(column) for column in GROUPING_COLUMNS],
                                    open = False)
                                ),
             open = True),
-        width = 400, title = "Peatland Code Dashboard"),
+        width = 420, title = "Peatland Code Dashboard"),
     ui.layout_columns(
         ui.layout_columns(
             ui.card(
@@ -70,8 +67,7 @@ userInterface = ui.page_sidebar(
         ui.navset_card_pill(
             ui.nav_panel("List"),
             ui.nav_panel("Map"),
-            title = "Projects"
-            ),
+            title = "Projects"),
         col_widths = [8, 4]),
     fillable = True)
 
